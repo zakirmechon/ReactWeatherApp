d1cd2ea70a2174ee8c7b7048ed464d85c3729fb8
Zakir Hussain <zakirmechon@gmail.com>
2018-06-14 01:49:24 +0530

    Components are Simplifed as Easy as Possible

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,16 +1,14 @@
 import React from "react";
 
-class Title extends React.Component{
-    render(){
-        return(
-            <div>
 
-                <h1>Weather Finder</h1>
-                <p> Find Out Temperature, Conditions and More.... </p>
+const Title = () => (
+    <div>
 
-            </div>
-        );
-    }
-};
+        <h1>Weather Finder</h1>
+        <p> Find Out Temperature, Conditions and More.... </p>
+
+    </div>
+);
+
 
 export default Title;
