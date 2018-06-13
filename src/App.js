002dfefc4a8664489cb155658b7f7fb5af29430d
Zakir Hussain <zakirmechon@gmail.com>
2018-06-14 00:35:02 +0530

    Basic Version on React Weather App

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+ const API_KEY = "7055fb803fe7eb7c53fc379c91c64dba";
+
+
+class App extends React.Component{
+  state = {
+    temperature:undefined,
+    city:undefined,
+    country:undefined,
+    humidity:undefined,
+    description:undefined,
+    error:undefined
+  }
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    if (city && country){
+      console.log(data);
+      this.setState({
+        temperature:data.main.temp,
+        city: data.name,
+        country:data.sys.country,
+        humidity:data.main.humidity,
+        description:data.weather[0].description,
+        error:""
+      });
+    }else{
+      this.setState({
+        temperature:undefined,
+        city:undefined,
+        country:undefined,
+        humidity:undefined,
+        description:undefined,
+        error:"Location Undefined : Please enter value in the text box"
+      });
+    }
+  }
+  render(){
+    return(
+      <div>
+        <Title />
+        <Form getWeather={this.getWeather}/>
+        <Weather 
+        temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Title extends React.Component{
+    render(){
+        return(
+            <div>
+
+                <h1>Weather Finder</h1>
+                <p> Find Out Temperature, Conditions and More.... </p>
+
+            </div>
+        );
+    }
+};
+
+export default Title;
